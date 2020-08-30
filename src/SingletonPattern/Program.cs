7326f90a2d726a4b3c50ecda5e430708940f96a4
sc.lin <read0@DESKTOP-HDLMPH8>
2020-08-30 16:06:13 +0800

    add thread-safe singleton

--- src/SingletonPattern/Program.cs
+++ src/SingletonPattern/Program.cs
@@ -36,6 +36,42 @@ namespace SingletonPattern
         }
     }
 
+    class SingletonThreadSafe
+    {
+       private static SingletonThreadSafe instance = null;
+        private static readonly object padlock = new object();
+
+        private SingletonThreadSafe()
+        {
+            System.Console.WriteLine("constructor new");
+        }
+
+        public static SingletonThreadSafe Instance
+        {
+            get
+            {
+                // if no lock, instance would be new twice.
+                // It is going to cause difference hash code between s12 and s22.
+                lock (padlock)
+                {
+                    Console.WriteLine("\nGet instance");
+                    if (instance == null)
+                    {
+                        Console.WriteLine("new one");
+                        instance = new SingletonThreadSafe();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+
+        public void doSomething()
+        {
+            Console.WriteLine("Do something~");
+        }
+ }
+
     class Program
     {
         static void Main(string[] args)
@@ -47,7 +83,21 @@ namespace SingletonPattern
             s2.doSomething();
 
 
-            //Console.WriteLine("\n\nTesting Singleton Pattern use multiple threading.");
+            Console.WriteLine("\n\nTesting Singleton Pattern use multiple threading.");
+            Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(5000);
+                SingletonThreadSafe s12 = SingletonThreadSafe.Instance;
+                Console.WriteLine($"first: {s12.GetHashCode()}");
+                s12.doSomething();
+            });
+            Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(5000);
+                SingletonThreadSafe s12 = SingletonThreadSafe.Instance;
+                Console.WriteLine($"second: {s12.GetHashCode()}");
+                s12.doSomething();
+            });
             Console.ReadLine();
         }
     }
