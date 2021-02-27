c5ca5e64566c4d55fcbbf7d0118270e605e911ed
sc.lin <read0@DESKTOP-HDLMPH8>
2021-02-27 21:03:46 +0800

    2021.02.27 New: Create abstract factory praticing project. New: Create simple factory praticing project.

---
+++ SimpleFactory/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    class Message
+    {
+        public Message()
+        { 
+        }
+
+        public void PeekMessage()
+        { 
+        }
+    }
+    class MessageFactory
+    {
+        public MessageFactory()
+        { 
+        }
+
+        public Message CreateMessage()
+        {
+            Message msg = new Message();
+            return msg;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
