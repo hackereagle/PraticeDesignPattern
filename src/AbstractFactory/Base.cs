805391fbb2d61e5c6d037bd5241cf2da5a26c577
sc.lin <read0@DESKTOP-HDLMPH8>
2021-03-02 23:36:39 +0800

    2021.03.02 sc.lin New: Create Base and concrete class for AbstractFactory.

---
+++ src/AbstractFactory/Base.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    interface IPoint
+    {
+        void Line(int width);
+    }
+
+    interface ICorner
+    {
+        void LeftUp();
+        void RightUp();
+        void LeftDown();
+        void RightDown();
+    }
+
+    interface IPointCornerFactory
+    {
+        IPoint GetPoint();
+        ICorner GetCorner();
+    }
+}
--- src/AbstractFactory/Program.cs
+++ src/AbstractFactory/Program.cs
@@ -6,6 +6,37 @@ using System.Threading.Tasks;
 
 namespace AbstractFactory
 {
+    class Rectangle
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        void Paint(IPointCornerFactory factory)
+        {
+            IPoint point = factory.GetPoint();
+            ICorner corner = factory.GetCorner();
+
+            corner.LeftUp();
+            point.Line(width - 2);
+            corner.RightUp();
+            Console.Write("\n");
+            for (int i = 0; i < height - 2, i++)
+            {
+                point.Line(width);
+                Console.Write("\n");
+            }
+            corner.LeftDown();
+            point.Line(width - 2);
+            corner.RightDown();
+            Console.Write("\n");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
