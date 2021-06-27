709aa10a982dab4ea6304b23df42a7247a070766
sc.lin <read0@DESKTOP-HDLMPH8>
2021-06-27 19:59:00 +0800

    2021.06.27 sc.lin New: Created strategy pattern practicing project.

---
+++ src/StrategyPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public interface IService
+    { 
+        void DoService()
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
