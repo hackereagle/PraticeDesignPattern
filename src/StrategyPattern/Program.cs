253c16b9fbaf85d1176b1e6ab5ee63367c071748
sc.lin <read0@DESKTOP-HDLMPH8>
2021-06-27 23:30:45 +0800

    2021.06.27 sc.lin Chg: Added Service and EchoService class into StrategyPattern project.

--- src/StrategyPattern/Program.cs
+++ src/StrategyPattern/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace StrategyPattern
 {
     public interface IService
-    { 
-        void DoService()
+    {
+        void DoService(Socket client);
     }
+
+    public class EchoService : IService
+    {
+        public void DoService(Socket client)
+        { 
+
+        }
+    }
+
+    public class Server
+    {
+        private Socket mServer;
+        private IService mService;
+        public Server(int port, IService service)
+        {
+        }
+
+        public void Start()
+        { 
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
