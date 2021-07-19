8abe1cbf7bb384b61dcf279ddb9d2873d5e4f798
sc.lin <read0@DESKTOP-HDLMPH8>
2021-07-19 23:32:08 +0800

    2021.07.19 sc.lin New: Created StatePattern_InGame project to pratice state pattern. The project imagine developing a game and the character having different behavior with different state.

---
+++ src/StatePattern_InGame/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern_InGame
+{
+    // This exercise refer to "七天學會設計模式" chapter 14
+
+    public class Warrior
+    {
+        private int mHp;
+        private State mState;
+        public Warrior()
+        {
+            this.mHp = 100;
+            this.mState = new NormalState();
+        }
+
+        public int HP { get { return this.mHp; } }
+
+        public void Heal(int heal)
+        {
+            this.mHp = this.mHp + heal;
+            if (this.mHp > 100)
+                this.mHp = 100;
+        }
+
+        public void GetDamage(int damage)
+        { 
+            this.mHp = this.mHp - damage;
+            if (this.mHp < 0)
+                this.mHp = 0;
+        }
+
+        public void Move()
+        {
+            this.mState.Move(this);
+        }
+
+        public void SetState(State state)
+        {
+            this.mState = state;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/StatePattern_InGame/State.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern_InGame
+{
+    /// <summary>
+    /// State changed by HP
+    /// </summary>
+    public interface State
+    {
+        /// <summary>
+        /// Different state responds different behavir, which relate warrior. 
+        /// So need pass warrior into State.
+        /// </summary>
+        /// <param name="warrior"></param>
+        void Move(Warrior warrior);
+    }
+
+    public class NormalState : State
+    {
+        public void Move(Warrior warrior)
+        {
+            if (warrior.HP > 70)
+            {
+                Console.WriteLine($"HP = {warrior.HP}, Normal State.");
+            }
+            else
+            {
+                warrior.SetState(new );
+                warrior.Move();
+            }
+        }
+    }
+}
